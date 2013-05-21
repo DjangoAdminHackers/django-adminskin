a10a080cc5e6c1f4fd212d95b7804fd82b417004
James Cleveland <jc@blackflags.co.uk>
2013-05-21 16:25:46 +0100

    fixing indentation

--- adminskin/static/admin/js/extensions.js
+++ adminskin/static/admin/js/extensions.js
@@ -4,76 +4,76 @@
 
 
 $(function ($) {
-        var initial_form = $.trim(unescape($.param($('form').serializeArray().sort(sort_by_name))).replace(/\s/g, ''));
-        var SAVING = false;
-        $('.submit-row').click(function() {
-            SAVING = true;
+    var initial_form = $.trim(unescape($.param($('form').serializeArray().sort(sort_by_name))).replace(/\s/g, ''));
+    var SAVING = false;
+    $('.submit-row').click(function() {
+        SAVING = true;
+    });
+    function sort_by_name(a, b){
+        var a_name = a.name.toLowerCase();
+        var b_name = b.name.toLowerCase();
+        return ((a_name < b_name) ? -1 : ((a_name > b_name) ? 1 : 0));
+    }
+
+    var check_changes = function() {
+        $('.filtered').filter('[id$=_to]').find('option').each(function(){
+            $(this).attr('selected', 'selected');
         });
-        function sort_by_name(a, b){
-            var a_name = a.name.toLowerCase();
-            var b_name = b.name.toLowerCase();
-            return ((a_name < b_name) ? -1 : ((a_name > b_name) ? 1 : 0));
+        var current_form = $.param($('form').serializeArray().sort(sort_by_name));
+        //Ignore the MCE effects
+        current_form = $.trim(unescape(current_form).replace(/\s/g, '').replace(/\+data-mce-href=".+?"/g, '').replace(/<p><br\+data-mce-bogus="1"><\/p>/g, ''));
+        if (current_form!=initial_form && !SAVING) {
+            return 'You have unsaved changes.';
         }
+    };
+    //Use serializeArray + param instead of serialize as we need to reorder
+    //the parameters
 
-        var check_changes = function() {
-            $('.filtered').filter('[id$=_to]').find('option').each(function(){
-                $(this).attr('selected', 'selected');
-            });
-            var current_form = $.param($('form').serializeArray().sort(sort_by_name));
-            //Ignore the MCE effects
-            current_form = $.trim(unescape(current_form).replace(/\s/g, '').replace(/\+data-mce-href=".+?"/g, '').replace(/<p><br\+data-mce-bogus="1"><\/p>/g, ''));
-            if (current_form!=initial_form && !SAVING) {
-                return 'You have unsaved changes.';
-            }
-        };
-        //Use serializeArray + param instead of serialize as we need to reorder
-        //the parameters
+    window.onbeforeunload = function() {
+        // filter horizontal
+        check_changes();
+    };
 
-        window.onbeforeunload = function() {
-            // filter horizontal
-            check_changes();
-        };
+    /* Move object tools next to h1 */
+    $("ul.object-tools").insertAfter("#content h1");
+    
+    /* Add show/hide filters toggle link */
+    $('#changelist-search').after('<a id="toggle" class="hide">Hide filters</a>');
+    
+    /* Show/hide filters on toggle link click */
+    $('#toggle').click(function(){
+        $('#changelist').toggleClass('hide-filters');
+        $('#toggle').toggleClass('show hide');
+        if ($('#toggle').hasClass('show')) {
+            $(this).text('Show filters');
+        } else {
+            $(this).text('Hide filters');
+        }
+    });
+    
+    /* Add cancel links to change form */
+    $('.change-form #content-main').before('<a href="../" class="cancel-header">Cancel</a>');
+    $('.change-form .submit-row').prepend('<a href="../" class="cancel-footer">Cancel</a>');
+    
+    /* Add id to search button */
+    $('#changelist-search input[type="submit"]').attr('id', 'search-submit');
+    
+    $('.move_to_submit_row').insertBefore('.submit-row>input:first');
+    $('.submit-row input').removeClass('default');
+    $('.submit-row>input:first').addClass('default');
 
-        /* Move object tools next to h1 */
-        $("ul.object-tools").insertAfter("#content h1");
-        
-        /* Add show/hide filters toggle link */
-        $('#changelist-search').after('<a id="toggle" class="hide">Hide filters</a>');
-        
-        /* Show/hide filters on toggle link click */
-        $('#toggle').click(function(){
-            $('#changelist').toggleClass('hide-filters');
-            $('#toggle').toggleClass('show hide');
-            if ($('#toggle').hasClass('show')) {
-                $(this).text('Show filters');
-            } else {
-                $(this).text('Hide filters');
-            }
-        });
-        
-        /* Add cancel links to change form */
-        $('.change-form #content-main').before('<a href="../" class="cancel-header">Cancel</a>');
-        $('.change-form .submit-row').prepend('<a href="../" class="cancel-footer">Cancel</a>');
-        
-        /* Add id to search button */
-        $('#changelist-search input[type="submit"]').attr('id', 'search-submit');
-        
-        $('.move_to_submit_row').insertBefore('.submit-row>input:first');
-        $('.submit-row input').removeClass('default');
-        $('.submit-row>input:first').addClass('default');
-
-        $('.submit-row-extra').insertBefore('.submit-row>input:first');
-        $('.submit-row-extra').on('click', function(event) {
-            event.preventDefault();
-            SAVING = false;
-            var $this = $(this);
-            var modal = $this.attr('modal');
-            var href = $this.attr('href');
-            if (modal !== undefined) {
-                $('#' + modal).foundation('reveal', 'open');
-            } else {
-                window.location = $(this).attr('href');
-                
-            }
-        });
+    $('.submit-row-extra').insertBefore('.submit-row>input:first');
+    $('.submit-row-extra').on('click', function(event) {
+        event.preventDefault();
+        SAVING = false;
+        var $this = $(this);
+        var modal = $this.attr('modal');
+        var href = $this.attr('href');
+        if (modal !== undefined) {
+            $('#' + modal).foundation('reveal', 'open');
+        } else {
+            window.location = $(this).attr('href');
+            
+        }
+    });
 });
